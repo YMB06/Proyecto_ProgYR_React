4291544368b22f1a8b5d95c1c06f7d45764c592b
YMB06 <youssefmf240@gmail.com>
2025-04-09 12:31:11 +0200

    configuracion inicial del router de clientes

--- CarRenting/src/routes/ClientRoutes.tsx
+++ CarRenting/src/routes/ClientRoutes.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import { ClientPage } from '../layouts/ClientPage'
 
 export const ClientRoutes = () => {
   return (
-    <div>ClientRoutes</div>
+    <Router>
+    <Routes>
+      <Route path="/admin" element={<ClientPage></ClientPage>} />
+      <Route path="/" element={<ClientPage></ClientPage>} />
+    </Routes>
+  </Router>  
   )
 }
